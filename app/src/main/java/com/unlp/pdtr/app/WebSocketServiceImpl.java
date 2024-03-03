128229bae4e4611a45ba2ee264ef7c0e2a0636ce
fots <facundotomatis@gmail.com>
2024-03-03 15:14:27 -0300

    add client implementation using socket.io lib

--- app/src/main/java/com/unlp/pdtr/app/WebSocketServiceImpl.java
+++ app/src/main/java/com/unlp/pdtr/app/WebSocketServiceImpl.java
@@ -3,16 +3,43 @@ package com.unlp.pdtr.app;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.time.Instant;
+
+import com.google.gson.Gson;
 import com.google.protobuf.Timestamp;
 import io.grpc.stub.StreamObserver;
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+import io.socket.engineio.client.Socket.Options;
+
 import com.unlp.pdtr.app.WebSocketServiceGrpc.WebSocketServiceImplBase;
 import com.unlp.pdtr.app.WebSocketServiceOuterClass.WebSocketRequest;
 import com.unlp.pdtr.app.WebSocketServiceOuterClass.WebSocketResponse;
 
 public class WebSocketServiceImpl extends WebSocketServiceImplBase {
+  private Socket socket;
   public WebSocketServiceImpl() throws URISyntaxException, InterruptedException {
-    MyWebSocket client = new MyWebSocket(new URI("ws://127.0.0.1:60200/"));
-    client.connectBlocking();
+    if (System.getenv("SOCKET_ENV") != null) {
+      socket = IO.socket("ws://frontend:60200");
+    } else {
+      socket = IO.socket("ws://localhost:60200");
+    }
+    socket.connect();
+    socket.on(Socket.EVENT_CONNECT_ERROR, new Emitter.Listener() {
+      @Override
+      public void call(Object... args) {
+        System.out.println("Error");
+        System.out.println(args[0].toString());
+      }
+    });
+    socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+      @Override
+      public void call(Object... args) {
+        System.out.println("Connected");
+        socket.emit("message", "Hello from Java");
+      }
+    });
+    System.out.println("Socket created");
   }
 
   @Override
@@ -22,7 +49,9 @@ public class WebSocketServiceImpl extends WebSocketServiceImplBase {
       @Override
       public void onNext(WebSocketRequest request) {
         System.out.println("Llega a WebSocketx");
-        // send a message to the server
+        Gson gson = new Gson();
+        String json = gson.toJson(request);
+        socket.emit("message", json);
         System.out.println(request.toString());
       }
 

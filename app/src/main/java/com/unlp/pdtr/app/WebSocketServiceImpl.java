814ae4a69a5eef2ae9bf3da658690fadbe2c2f8e
fots <facundotomatis@gmail.com>
2024-03-02 23:35:39 -0300

    added server client ws connection

--- app/src/main/java/com/unlp/pdtr/app/WebSocketServiceImpl.java
+++ app/src/main/java/com/unlp/pdtr/app/WebSocketServiceImpl.java
@@ -1,5 +1,7 @@
 package com.unlp.pdtr.app;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.time.Instant;
 import com.google.protobuf.Timestamp;
 import io.grpc.stub.StreamObserver;
@@ -7,37 +9,41 @@ import com.unlp.pdtr.app.WebSocketServiceGrpc.WebSocketServiceImplBase;
 import com.unlp.pdtr.app.WebSocketServiceOuterClass.WebSocketRequest;
 import com.unlp.pdtr.app.WebSocketServiceOuterClass.WebSocketResponse;
 
-public class WebSocketServiceImpl extends WebSocketServiceImplBase
-{
-    @Override
-    public StreamObserver<WebSocketRequest> showContent(final StreamObserver<WebSocketResponse> responseObserver) {
+public class WebSocketServiceImpl extends WebSocketServiceImplBase {
+  public WebSocketServiceImpl() throws URISyntaxException, InterruptedException {
+    MyWebSocket client = new MyWebSocket(new URI("ws://127.0.0.1:60200/"));
+    client.connectBlocking();
+  }
 
-        return new StreamObserver<WebSocketRequest>() {
-            @Override
-            public void onNext(WebSocketRequest request) {
-                //TODO show in webpage
-                System.out.println("Llega a WebSocket");
-                System.out.println(request.toString());
-            }
+  @Override
+  public StreamObserver<WebSocketRequest> showContent(final StreamObserver<WebSocketResponse> responseObserver) {
 
-            @Override
-            public void onError(Throwable t) {
-                System.out.println(t.toString());
-            }
+    return new StreamObserver<WebSocketRequest>() {
+      @Override
+      public void onNext(WebSocketRequest request) {
+        System.out.println("Llega a WebSocketx");
+        // send a message to the server
+        System.out.println(request.toString());
+      }
 
-            @Override
-            public void onCompleted() {
-                //TODO setear timestamp de ultima entrada cargada
-                Instant currentTimestamp = Instant.now();
-                Timestamp time = Timestamp.newBuilder()
-                        .setSeconds(currentTimestamp.getEpochSecond())
-                        .setNanos(currentTimestamp.getNano())
-                        .build();
+      @Override
+      public void onError(Throwable t) {
+        System.out.println(t.toString());
+      }
 
-                WebSocketResponse response = WebSocketResponse.newBuilder().setTime(time).build();
-                responseObserver.onNext(response);
-                System.out.println("SHOW OPERATION FINISHED");
-            }
-        };
-    }
+      @Override
+      public void onCompleted() {
+        //TODO setear timestamp de ultima entrada cargada
+        Instant currentTimestamp = Instant.now();
+        Timestamp time = Timestamp.newBuilder()
+                         .setSeconds(currentTimestamp.getEpochSecond())
+                         .setNanos(currentTimestamp.getNano())
+                         .build();
+
+        WebSocketResponse response = WebSocketResponse.newBuilder().setTime(time).build();
+        responseObserver.onNext(response);
+        System.out.println("SHOW OPERATION FINISHED");
+      }
+    };
+  }
 }
